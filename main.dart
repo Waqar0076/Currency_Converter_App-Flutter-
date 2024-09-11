a9c9b3a5e1d137aaff02083c86c2cfb26994b85a
Waqar Aamir <113366062+Waqar0076@users.noreply.github.com>
2024-09-11 20:42:51 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:currency_converter/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainPage());
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
